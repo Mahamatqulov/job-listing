2d9b39704aad2e948bd75b14501a95394274d175
bahrom <baxrom@email.com>
2025-02-06 13:56:41 +0500

    update

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,11 +3,14 @@ import React from "react";
 function Navbar() {
   return (
     <div className="bg-[#5CA5A5]">
-      <img
-        className="w-full  h-[150px]"
-        src="./bg-header-desktop.svg"
-        alt="Background Header"
-      />
+      <picture>
+        <source media="(max-width: 640px)" srcSet="./bg-header-mobile.svg" />
+        <img
+          className="w-full h-auto"
+          src="./bg-header-desktop.svg"
+          alt="Background Header"
+        />
+      </picture>
     </div>
   );
 }
